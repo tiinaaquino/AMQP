316c7fdbdc03057234e695e174dac736f88f7759
Tina <tiinaaquino@users.noreply.github.com>
2018-04-16 10:12:34 -0700

    Add files via upload

---
+++ consumer.py
@@ -0,0 +1,15 @@
+import pika
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(host = 'localhost'))
+channel = connection.channel()
+
+channel.queue_declare(queue = 'hello')
+
+def callback(ch, method, properties, body):
+	print(" [x] Received message: %r" % body)
+
+
+channel.basic_consume(callback, queue = 'hello', no_ack = True)
+
+print(' [*] Waiting for messages. Press CTRL+C to exit.')
+channel.start_consuming()
